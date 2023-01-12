fc426d2556b1b2a4eb02e769bf372224cf73e20a
Eldar Kodzaev <eldar_kodzaev@gmail.com>
2023-01-12 18:45:41 +0300

    Создал структуру проекта и написал пустые сервисы-заглушки

---
+++ app_order/services.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Order:
+    def history(self) -> List:
+        """
+        История заказов
+        :return: список заказов
+        :rtype: list
+        """
+        pass
+
+    def payment(self) -> None:
+        """
+        Оплата заказа
+        :return: None
+        :rtype: None
+        """
+        pass
---
+++ app_shop/services.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+def viewed_products() -> List:
+    """
+    Список просмотренных товаров
+    :return: список просмотренных товаров
+    :rtype: list
+    """
+    pass
+
+
+def add_review_to_product(product: object, review: str) -> None:
+    """
+    Добавление отзыва к товару
+    :param product: товар
+    :type product: object
+    :param review: текст отзыва
+    :type review: str
+    :return: None
+    :rtype: None
+    """
+    pass
+
+
+def import_products(folder_path: str) -> None:
+    """
+    Импорт товаров из XML или JSON-файлов
+    :param folder_path: путь к папке с файлами для импорта
+    :type folder_path: str
+    :return: None
+    :rtype: None
+    """
+    pass
