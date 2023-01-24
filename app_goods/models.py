0a00ff2e96c26d864035664739014a4f454c5310
Максим Семенюк <expmax85@gmail.com>
2023-01-24 23:06:07 +0500

    add category

--- app_goods/models.py
+++ app_goods/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from app_category.models import Category
-from app_shop.models import Shops
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=40)
 
 
 class Items(models.Model):
