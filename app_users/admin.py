18a8957537c865af8f2099ba63d66e985cff6ab5
Багаудин Абдулмеджидов <bagaudin200@mail.ru>
2023-01-26 15:44:16 +0300

    new change

--- app_users/admin.py
+++ app_users/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .models import User, ProxyGroups
+from .models import User
 
 
 class UserAdmin(admin.ModelAdmin):
     pass
 
 admin.site.unregister(Group)
-admin.site.register(ProxyGroups)
 admin.site.register(User, UserAdmin)
