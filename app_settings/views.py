1b8ab777f4ecba4875ed8ee540ecb3cb7835bfc3
Александр <alexx.dv.ar@gmail.com>
2023-02-13 15:00:30 +0300

    Создал приложение настроек сайта

---
+++ app_settings/views.py
@@ -0,0 +1,14 @@
+
+from django.contrib import messages
+from django.core.cache import cache
+from django.shortcuts import redirect
+
+
+def clear_all_cache_view(request):
+    """Очистка всего кеша"""
+    cache.clear()
+    messages.success(request, 'All cache is cleared')
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+
