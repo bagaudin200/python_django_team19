9ec712f26ac7b4c7dd8132ac9adaf23ed59e4d9f
Alena Andrievskaya <bianco.neve.2017@gamil.com>
2023-02-15 15:30:17 +0500

    product.jinja2 corrected

--- app_goods/forms.py
+++ app_goods/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from .models import Reviews
+from .models import Review
 
 
 class Reviewsform(forms.ModelForm):
 
     class Meta:
-        model = Reviews
-        fields = ('comment',)
+        model = Review
+        fields = ('text',)
 
 
