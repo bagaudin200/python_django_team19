13fb70b74272745889c6e45c384b1bcfaadf1a90
Багаудин Абдулмеджидов <bagaudin200@mail.ru>
2023-02-16 07:33:34 +0300

    popular

--- app_goods/urls.py
+++ app_goods/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GoodsDetailView
+from .views import GoodsDetailView, ShopView
 
 urlpatterns = [
     path('product/<slug:product_slug>/', GoodsDetailView.as_view(), name='product'),
+    path('top/', ShopView.as_view(), name='top')
 ]
