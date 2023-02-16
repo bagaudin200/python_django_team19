582e04cc39b9783ac543dde2fb80dfbe6e48e4c0
Багаудин Абдулмеджидов <bagaudin200@mail.ru>
2023-02-16 07:38:17 +0300

    popular

--- app_goods/models.py
+++ app_goods/models.py
@@ -29,36 +29,54 @@ class Category(MPTTModel):
         return f'{self.name}'
 
 
-class Items(models.Model):
-    name = models.CharField(max_length=100, verbose_name='name')
+class Product(models.Model):
+    """Модель товара"""
+    name = models.CharField(max_length=100, db_index=True, verbose_name='name')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, max_length=100, verbose_name='category')
-    image = models.ImageField(upload_to='static/', verbose_name='image')
     description = models.CharField(max_length=1000, verbose_name='description')
-    price = models.DecimalField(max_digits=10, decimal_places=0, default=0, verbose_name='price')
-    number = models.PositiveIntegerField(default=0, verbose_name='number')
+    is_limited = models.BooleanField(default=False, verbose_name='is limited')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='price')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='quantity')
+    image = models.ImageField(upload_to=product_directory_path, blank=True, null=True, verbose_name='image')  # основное фото
 
     class Meta:
-        verbose_name = 'item'
-        verbose_name_plural = 'items'
+        db_table = 'product'
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
 
     def __str__(self):
         return f'{self.name}'
 
+    def get_absolute_url(self):
+        return reverse('product', kwargs={'product_slug': self.slug})
 
-class Reviews(models.Model):
 
-    user = models.ForeignKey('app_users.User', default=None, null=True, on_delete=models.CASCADE, related_name='user',
-                             verbose_name='user')
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
-    comment = models.CharField(max_length=1000, default='', verbose_name='comment')
-    item = models.ForeignKey(Items, max_length=100, on_delete=models.CASCADE, related_name='reviews',
-                             verbose_name='item')
-    create_date = models.DateTimeField(auto_now_add=True, verbose_name='date')
+class Image(models.Model):
+    """Модель фотографии товара"""
+    image = models.ImageField(upload_to=product_directory_path, blank=True, null=True, verbose_name='image')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images', verbose_name='product')
 
     class Meta:
+        db_table = 'image'
+        verbose_name = 'image'
+        verbose_name_plural = 'images'
+
+    def __str__(self):
+        return self.product.name
+
+
+class Review(models.Model):
+    """Модель отзыва"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews', verbose_name='user')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews', verbose_name='product')
+    text = models.TextField(verbose_name='text')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created at')
+
+    class Meta:
+        db_table = 'review'
         verbose_name = 'review'
         verbose_name_plural = 'reviews'
 
     def __str__(self):
-        return f'{self.item}'
+        return f"Review for {self.product} by {self.user}"
