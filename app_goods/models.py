6ee9d6d37ef2e59176399ff295eed0bafbac14ff
Багаудин Абдулмеджидов <bagaudin200@mail.ru>
2023-02-16 07:36:14 +0300

    popular

--- app_goods/models.py
+++ app_goods/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='name')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    image = models.ImageField(blank=True, upload_to='category/', verbose_name='Изображение')
 
     class Meta:
         verbose_name = 'category'
