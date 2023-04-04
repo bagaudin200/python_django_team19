8b392a00a3abbec08231cf28a58faaa60c24a0f8
Александр <alexx.dv.ar@gmail.com>
2023-04-04 19:29:02 +0300

    Реализация пространства имен

--- app_goods/views.py
+++ app_goods/views.py
@@ -10,9 +10,10 @@ from app_cart.services import CartServices
 from app_goods.forms import AddProductToCardForm, ReviewsForm
 from app_goods.models import Product
 from app_goods.services.catalog_services import CatalogPaginator, CatalogQueryStringBuilder, CatalogQuerySetBuilder
+from app_settings.models import SiteSettings
 from .forms import FilterForm
 from .services.home_page_services import HomePageServices
-from .services.services import check_product_quantity, get_update_quantity_product, ReviewService
+from .services.product_services import ReviewService, ProductService
 
 
 class GoodsDetailView(DetailView):
@@ -28,21 +29,18 @@ class GoodsDetailView(DetailView):
         obj = cache.get(f"product:{slug}")
         if not obj:
             obj = super(GoodsDetailView, self).get_object()
-            cache.set(f"product:{slug}", obj)
+            cache.set(f"product:{slug}", obj, SiteSettings.load().product_cache_time)
         return obj
 
     def post(self, request, *args, **kwargs):
         form = AddProductToCardForm(request.POST)
         if form.is_valid():
-            user = self.request.user
-            product = self.get_object()
+            product_service = ProductService(self.request, self.get_object(), kwargs['slug'])
             quantity = form.cleaned_data['quantity']
-            if check_product_quantity(product=product, quantity=quantity):
-                update_product = get_update_quantity_product(product=product,
-                                                             user=user
-                                                             )
+            if product_service.check_product_quantity(quantity=quantity):
+                update_product = product_service.get_update_quantity_product()
                 cart_services = CartServices(request)
-                cart_services.add(product=product,
+                cart_services.add(product=self.get_object(),
                                   quantity=quantity,
                                   update_quantity=update_product,
                                   )
@@ -53,24 +51,23 @@ class GoodsDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        product = self.object
-        review_service = ReviewService(self.request.user)
-        context['images'] = product.images.all()
-        context['tags'] = product.tags.all()
-        context['reviews'] = review_service.get_reviews_for_product(product)
+        review_service = ReviewService(self.request.user, self.get_object())
+        product_service = ProductService(self.request, self.get_object(), kwargs['object'].slug)
+        context['images'] = product_service.get_images()
+        context['tags'] = self.object.tags.all()
+        context['reviews'] = review_service.get_reviews_for_product()
         context['product_form'] = AddProductToCardForm()
         context['review_form'] = ReviewsForm()
         return context
 
 
 def add_review(request):
-    print(request.POST)
     if request.method == 'POST':
         form = ReviewsForm(request.POST)
         if form.is_valid():
-            review = ReviewService(request.user)
+            review = ReviewService(request.user, request.POST['product'])
             text = form.cleaned_data['text']
-            review.add(product=request.POST['product'], review=text)
+            review.add(review=text)
     return redirect(request.META.get('HTTP_REFERER'))
 
 
